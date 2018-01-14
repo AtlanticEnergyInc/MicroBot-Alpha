31da12718b00f3491e041df7f29a4561da72c645
Joshua <djjosh.net@gmail.com>
2018-01-14 18:48:39 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 pip install -r requirements.txt
-OAUTH2_CLIENT_ID=152638009253036032 OAUTH2_CLIENT_SECRET=p6KNMamrU5OVWtZiFe2kSkhx3Amxm0xB python app.py
+OAUTH2_CLIENT_ID=358403251424722945 OAUTH2_CLIENT_SECRET=MzU4NDAzMjUxNDI0NzIyOTQ1.DT19Sg.iacF2hyG0A4Bkb-tfaLnuyqvsZs python app.py
 open http://localhost:5000
